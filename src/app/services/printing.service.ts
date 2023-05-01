c5540b9afbecd528b4acabf56366b41f76477feb
Kumar Saptam <45693173+Sapython@users.noreply.github.com>
2023-05-01 08:17:26 +0530

    Update printing.service.ts

--- src/app/services/printing.service.ts
+++ src/app/services/printing.service.ts
@@ -236,4 +236,58 @@ export class PrintingService {
     return window.pywebview.api.print('reprintKot',kotdata,printerConfig);
   }
 
+  printEditedKot(kot:KotConstructor,oldProducts:Product[],table:string,billNo:string){
+    if(!window.pywebview.api) return;
+    let businessDetails = {
+      'name': this.dataprovider.currentBusiness?.hotelName,
+      'address': this.dataprovider.currentBusiness?.address,
+      'phone': this.dataprovider.currentBusiness?.phone,
+      "gst": this.dataprovider.currentBusiness?.gst,
+      "fssai": this.dataprovider.currentBusiness?.fssai,
+    }
+    let printerConfig = this.dataprovider.currentMenu?.products.map((product:any)=>{
+      return {product:product.id,printer:product.category.printer}
+    })
+    let products:Product[] = []
+    products = oldProducts.map((product:any)=>{
+      product.edited = true
+      return product
+    })
+    let totalQuantity = 0;
+    kot.products.forEach((product) => {
+      let index = products.findIndex((item) => item.id === product.id);
+      if(index !== -1){
+        products[index].quantity += product.quantity;
+        totalQuantity += product.quantity;
+      } else {
+        products.push(product);
+        totalQuantity += product.quantity;
+      }
+    })
+    let kotdata = {
+      "id": kot.id,
+      "products":products.map((product:Product)=>{
+        return {
+          "id":product.id,
+          "name":product.name,
+          "instruction":product.instruction,
+          "quantity":product.quantity,
+          "price":product.price,
+          "total":product.price*product.quantity,
+        }
+      }),
+      "date":(new Date()).toLocaleDateString(),
+      "time":(new Date()).toLocaleTimeString(),
+      "totalQuantity":totalQuantity,
+      "cashierName": this.dataprovider.currentUser?.name,
+      "mode":"kot",
+      "table": table,
+      'billNo': billNo,
+      'notes':[],
+      'businessDetails': businessDetails
+    }
+    console.log("printing data",kotdata,printerConfig);
+    return window.pywebview.api.print('reprintKot',kotdata,printerConfig);
+  }
+
 }
