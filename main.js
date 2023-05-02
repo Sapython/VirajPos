b6a50913d7e35c586874d4a416d154a78559cca8
Kumar Saptam <45693173+Sapython@users.noreply.github.com>
2023-05-02 09:20:38 +0530

    Added electron

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  console.log(path.join(__dirname, "./dist/viraj/index.html"))
+  win.loadFile("./dist/viraj/index.html");
+  win.webContents.on("did-fail-load", () => {
+    console.log("did-fail-load");
+    win.loadURL(
+        path.join('file://',__dirname, "./dist/viraj/index.html"),
+    );
+    // REDIRECT TO FIRST WEBPAGE AGAIN
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
