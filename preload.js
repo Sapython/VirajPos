38346244b5c42d27825c507f18ce95ee75e35336
chinus1998 <104675814+chinus1998@users.noreply.github.com>
2023-05-05 22:01:31 +0530

    Fixed billing and added bill printing setup.

--- preload.js
+++ preload.js
@@ -1,34 +1,4 @@
 // const { contextBridge } = require('electron');
-// const { exec } = require('child_process');
-// var htmlToPdf = require('html-to-pdf');
-// const fs = require('fs');
-// const path = require('path');
-// const { print } = require("pdf-to-printer");
-// var html_to_pdf = require('html-pdf-node');
-// htmlToPdf.setDebug(true);
-// console.log("Loaded");
-// // print(path.join(__dirname, "./posCore/kot/28-04-2023_23-42-58.pdf")).then(console.log);
-// function myPrint(){
-//   // open as file object  path.join(__dirname, "./posCore/templates/index.html")
-//   fs.readFile(path.join(__dirname, "./posCore/templates/index.html"), 'utf8', function(err, data) {
-//     if (err) {
-//       return console.log(err);
-//     }
-//     let file = { content: "<p>Test data</p>" };
-//     console.log(file);
-//     // let file = { content:  };
-//     // let file = { url: };
-//     return html_to_pdf.generatePdf(file, {width:'80mm'})
-//   })
-// }
-// contextBridge.exposeInMainWorld('printing', {
-//   print: myPrint,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron,
-//   // we can also expose variables, not just functions
-// })
-
-
 
 // New code implmentation
 
@@ -38,9 +8,6 @@ let EscPosEncoder = require('esc-pos-encoder');
 class customEncoder extends EscPosEncoder {
   initPrint(){
     this.initialize()
-    .newline()
-    .newline()
-    .newline()
     return this
   }
   h1(text){
@@ -48,11 +15,14 @@ class customEncoder extends EscPosEncoder {
     .width(2)
     .size('small').align('center')
     .line(text)
+    .newline()
+    .size('normal').align('left').width(1).height(1)
+  }
+  h2(text,align='center'){
+    return this.bold(true).align(align).size('normal').line(text).bold(false).align('left')
   }
   hr(double = false){
-    return this.height(1)
-    .width(1)
-    .rule({style:double ? 'double' : 'single'})
+    return this.rule({style:double ? 'double' : 'single'})
   }
   productTable(products){
     // products contains name, price, quantity, total
@@ -60,96 +30,333 @@ class customEncoder extends EscPosEncoder {
       { width: 26, marginRight: 2, align: 'left' },
       { width: 5, marginRight: 2, align: 'center' },
       { width: 5, marginRight: 2, align: 'center' },
-      { width: 10, align: 'right' }
+      { width: 5, align: 'right' }
     ]
     let data = []
+    data.push([
+      (encoder) => encoder.bold().text('Item').bold(),
+      (encoder) => encoder.bold().align('center').text('Qty').bold(),
+      (encoder) => encoder.bold().align('center').text('Price').bold(),
+      "Amount"
+    ])
     products.forEach(product => {
-      data.push([product.name, product.quantity, product.price, product.total])
+      data.push([product.name, product.quantity.toString(), "Rs."+product.price.toString(), "Rs."+product.amount.toString()])
     });
     return this.table(table, data)
   }
+  lineIf(text,align = "left",prefix=null){
+    if (text){
+      return this.align(align).line((prefix ? prefix : '')+text)
+    } else {
+      return this
+    }
+  }
+  end(id){
+    return this.newline()
+    .newline()
+    .align('center')
+    .qrcode('https://fbms-shreeva-demo.web.app/'+id)
+    .align('left')
+    .newline()
+    .newline()
+    .newline()
+    .newline()
+    .newline()
+    .cut()
+    .encode()
+  }
+  terms(terms){
+    // terms is of type string[]
+    this.align('center').line('Terms & Conditions').align('left').newline()
+    terms.forEach((term,index) => {
+      this.line((index+1).toString()+': '+term)
+    })
+    return this
+  }
+
+  discounts(disocunts){
+    // discounts is of type {name: string, value: number, type: string, rate: number}[]
+    this.align('center').h2('Discounts','left')
+    let discountsColumns = [['Discount','Rate','Amount']]
+    disocunts.forEach((discount,index) => {
+      discountsColumns.push([discount.name, discount.rate.toString()+'%',"Rs."+discount.value.toString()])
+    })
+    return this.table([
+        { width: 20, marginRight: 2, align: 'left' },
+        { width: 10, marginRight: 2, align: 'center' },
+        { width: 10, align: 'right' }
+      ],discountsColumns
+    ).newline()
+  }
+
+  taxes(taxes){
+    // taxes is of type {name: string, value: number, rate: number}[]
+    this.align('center').h2('Taxes','left')
+    let taxesColumns = [['Tax','Rate','Amount']]
+    taxes.forEach((tax,index) => {
+      taxesColumns.push([tax.name, tax.rate.toString()+'%',"Rs."+tax.value.toString()])
+    })
+    return this.table([
+        { width: 20, marginRight: 2, align: 'left' },
+        { width: 10, marginRight: 2, align: 'center' },
+        { width: 10, align: 'right' }
+      ],taxesColumns
+    ).newline()
+  }
 }
 console.log(printer.getPrinters());
-
-let encoder = new customEncoder({width:75});
-// cp437, cp720, cp737, cp775, cp850, cp851, cp852, cp853, cp855, cp857, cp858, cp860, cp861, cp862, cp863, cp864, cp865, cp866, cp869, cp874, cp922, cp1098, cp1118, cp1119, cp1125, cp2001, cp3001, cp3002, cp3011, cp3012, cp3021, cp3041, cp3840, cp3841, cp3843, cp3844, cp3845, cp3846, cp3847, cp3848, iso885915, iso88592, iso88597, rk1048, windows1250, windows1251, windows1252, windows1253, windows1254, windows1255, windows1256, windows1257, windows1258,
-let result = encoder
-.initPrint()
-.h1("Hotel name")
-.width(2)
-.size('small').align('center')
-.line('Hotel Name')
-.newline()
-.height(1)
-.width(1)
-.table(
-  [
-      { width: 36, marginRight: 2, align: 'left' },
-      { width: 10, align: 'right' }
-  ], 
-  [
-      [ 'Item 1', '€ 10,00' ],
-      [ 'Item 2', '15,00' ],
-      [ 'Item 3', '9,95' ],
-      [ 'Item 4', '4,75' ],
-      [ 'Item 5', '211,05' ],
-      [ '', '='.repeat(10) ],
-      [ 'Total', (encoder) => encoder.bold().text('€ 250,75').bold() ],
+let businessDetails = {
+  name: 'Viraj',
+  address: 'Near Viraj',
+  phone: '1234567890',
+  fssai: 'FSSAI1234567890',
+  gst: 'GSTI1234567890',
+};
+let billdata = {
+  id: 1,
+  products: [
+    {
+      id: '1',
+      name: 'Product 1 kdjhsfjhsdkfjhskjdhfkjshdjkh',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: 30,
+      quantity: 2,
+      amount: 60,
+    },
+  ],
+  date: new Date().toLocaleDateString(),
+  time: new Date().toLocaleTimeString(),
+  grandTotal: 1000,
+  tax: [
+    {
+      name: 'CGST',
+      value: 50,
+      rate: 5,
+    },
+    {
+      name: 'CGST',
+      value: 50,
+      rate: 5,
+    },
+  ],
+  totaltax: {
+    value: 100,
+    rate: 10,
+  },
+  discount: [
+    {
+      name: 'ZomatoPro',
+      value: 10,
+      type: 'percentage',
+      rate: 5,
+    },
+    {
+      name: 'Veena',
+      value: 50,
+      type: 'percentage',
+      rate: 5,
+    },
+  ],
+  subtotal: 1000,
+  totalQuantity: 5,
+  cashierName: 'Neeraj',
+  mode: 'takeaway',
+  table: 1,
+  billNo: 1,
+  tokenNo: 1,
+  customerDetail: {
+    name: 'Neeraj',
+    phone: '1234567890',
+    address: 'Near Viraj',
+    gst: 'GST1234567890',
+  },
+  note: 'This is a special note',
+  notes: [
+    "This is a note",
+    "This is a note",
+    "This is a note",
+    "This is a note",
+  ],
+  businessDetails: businessDetails,
+};
+kotData ={
+  'id':1,
+  'businessDetails': businessDetails,
+  "table": 1,
+  'billNo': 1,
+  "date": new Date().toLocaleDateString(),
+  "time": new Date().toLocaleTimeString(),
+  "mode":"firstChargeable",
+  "products":[
+      {
+          "id":"1",
+          "name":"Item 1 kfjhdskjfhkjdshfjsdkfjhsdkjhkjdhfkjsdhfkjhsdkfjhs",
+          "instruction":"",
+          "quantity":2,
+      },
+      {
+          "id":"1",
+          "name":"Item 2",
+          "instruction":"spicy",
+          "quantity":2,
+      },
+      {
+          "id":"1",
+          "name":"Item 3",
+          "instruction":"",
+          "quantity":2,
+      },
+      {
+          "id":"1",
+          "name":"Item 4",
+          "instruction":"",
+          "quantity":2,
+      },
   ]
-)
-.newline()
-.newline()
-.newline()
-.newline()
-    .cut()
-    .encode();
-printer.printDirect({data:result // or simple String: "some text"
-  //, printer:'Foxit Reader PDF Printer' // printer name, if missing then will print to default printer
-  , type: 'RAW' // type: RAW, TEXT, PDF, JPEG, .. depends on platform
-  , success:function(jobID){
-    console.log("sent to printer with ID: "+jobID);
-  }
-  , error:function(err){console.log(err);},
-  printer:'POS-80C'
-});
+}
+function printBill(billdata){
+  let encoder = new customEncoder({width:48});
+  let result = encoder
+  .initPrint()
+  .h1(billdata.businessDetails.name)
+  .lineIf(billdata.businessDetails.address,'center',"Add: ",)
+  .lineIf(billdata.businessDetails.phone,'center',"Phone: ",)
+  .lineIf(billdata.businessDetails.fssai,'center',"FSSAI: ",)
+  .lineIf(billdata.businessDetails.gst,'center',"GST: ",)
+  .hr()
+  .h2("Customer details",'left')
+  .lineIf(billdata.customerDetail.name,'left','Name:')
+  .lineIf(billdata.customerDetail.phone,'left','Phone:')
+  .lineIf(billdata.customerDetail.address,'left','Add:')
+  .lineIf(billdata.customerDetail.gst,'left','Gst:')
+  .hr()
+  .table(
+    [
+      {
+        width:20,
+        marginRight:2,
+        align:'left'
+      },
+      {
+        width:20,
+        align:'right'
+      },
+    ],[
+      ['Date: '+billdata.date,'Time: '+billdata.time],
+      ['Token: '+billdata.tokenNo,'Bill: '+billdata.billNo],
+      ['Cashier: '+billdata.cashierName,'Mode: '+billdata.mode],
+    ]
+  )
+  .hr()
+  .productTable(billdata.products)
+  .hr()
+  .table(
+    [
+        { marginRight: 2, align: 'left' },
+        { align: 'right' }
+    ], 
+    [
+        [ 
+          'Total Qty:'+billdata.totalQuantity, 
+           (encoder) => encoder.bold().text("Sub: Rs."+billdata.subtotal).bold()  
+        ],
+    ]
+  )
+  .hr()
+  .discounts(billdata.discount)
+  .hr()
+  .taxes(billdata.tax)
+  .hr(true)
+  .table(
+    [
+        { marginRight: 2, align: 'left' },
+        { align: 'right' }
+    ], 
+    [
+        [ (enc)=> enc.bold(true).text("Grand Total: "), (encoder) => encoder.bold(true).height(2).width(2).size('normal').text("Rs."+billdata.grandTotal).bold(false).width(1).height(1) ],
+    ]
+  )
+  .hr()
+  .h2(billdata.note)
+  .terms(billdata.notes)
+  .end();
+  var promiseResolve, promiseReject;
+  var promise = new Promise(function(resolve, reject){
+    promiseResolve = resolve;
+    promiseReject = reject;
+  });
+  printer.printDirect({
+    data:result,
+    success:promiseResolve,
+    error:promiseReject,
+    printer:'POS-80C'
+  });
+  return promise;
+}
 
-// const escpos = require('escpos');
-// // install escpos-usb adapter module manually
-// let usbHandler = require('escpos-usb');
-// let printer =usbHandler.findPrinter()
-// console.log(printer[0].deviceDescriptor.idVendor)
-// usbHandler.getDevice(printer[0].deviceDescriptor.idVendor,printer[0].deviceDescriptor.idProduct)
-// // Select the adapter based on your printer type
-// // console.log("Loading USB");
-// // const device  = new escpos.USB();
-// // // const device  = new escpos.Network('localhost');
-// // // const device  = new escpos.Serial('/dev/usb/lp0');
- 
-// // // const options = { encoding: "GB18030" /* default */ }
-// // // encoding is optional
-// // console.log("printer",device);
-// // const printer = new escpos.Printer(device);
- 
-// // device.open(function(error){
-// //   printer
-// //   .font('a')
-// //   .style('bu')
-// //   .align('ct')
-// //   .size(1, 1)
-// //   .text('The quick brown fox jumps over the lazy dog')
-// //   .text('敏捷的棕色狐狸跳过懒狗')
-// //   .barcode('1234567', 'EAN8')
-// //   .table(["One", "Two", "Three"])
-// //   .tableCustom(
-// //     [
-// //       { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-// //       { text:"Center", align:"CENTER", width:0.33},
-// //       { text:"Right", align:"RIGHT", width:0.33 }
-// //     ],
-// //     { encoding: 'cp857', size: [1, 1] } // Optional
-// //   )
-// //   .qrimage('https://github.com/song940/node-escpos', function(err){
-// //     this.cut();
-// //     this.close();
-// //   });
-// // });
+function printKot(kotData){
+  let encoder = new customEncoder({width:75});
+  let result = encoder
+  .initPrint()
+  .h1()
+}
+printBill(billdata)
+// contextBridge.exposeInMainWorld('printing', {
+//   printBill: () => printBill,
+//   printKot: () => printKot,
+// })
